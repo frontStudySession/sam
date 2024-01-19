a1812f2ddeabaf3b11795bcfb9d78922c6df36f4
Sam <needermg@gmail.com>
2024-01-19 13:21:16 +0900

    Feat: alias 설정 및 styled components 적용

---
+++ src/app/hello.tsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const Hello = ()=> <Title>hello</Title>
+
+const Title = styled.div`
+  font-size: 1.5em;
+  text-align: center;
+  color: #BF4F74;
+`
