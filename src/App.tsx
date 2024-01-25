b29c73c4c402ceae116c00f2cafc5361e74b035a
samankey <71953671+samankey@users.noreply.github.com>
2024-01-25 18:01:09 +0900

    Feat: react-hook-form 테스트 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css';
 import { Hello } from '@app/app/Hello';
+import Form from '@app/app/Form';
 
 function App() {
   return (
     <div className="App">
       <Hello />
+      <Form />
     </div>
   );
 }
